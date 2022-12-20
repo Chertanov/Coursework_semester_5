b2162a591e7fc13e0176d7d0516d67d9aeee76c1
Chertanov <yaroslav.chertanov@nure.ua>
2022-12-21 01:47:31 +0200

    first commit

---
+++ courseworkGUI.ps1
@@ -0,0 +1,204 @@
+﻿Add-Type -assembly System.Windows.Forms
+
+$name = "C:\users"
+$starter = get-childitem -Path $name
+
+
+function Get-FileorDirectorySize($item){
+if($item/1GB -ge 1){
+$item = [string]([math]::round(($item/1GB),2)) + " GB"
+}elseif($item/1MB -ge 1)
+{$item = [string]([math]::round(($item/1MB),2)) + " MB"}
+else{$item = [string]([math]::round(($item/1KB),2)) + " KB"}
+return $item
+}
+
+
+function Count-FileExtensions($items){
+ForEach ($item in $items){
+    if($item.Extension -like ""){
+        if(".dir"  -notin $filetypes.keys){
+            $filetypes[".dir"] = 1 
+        }else{
+            $filetypes[".dir"] += 1}
+    }
+    elseif($item.Extension  -notin $filetypes.keys){
+        $filetypes[$item.Extension] = 1 
+    }else{
+        $filetypes[$item.Extension] += 1
+    }
+}
+return $filetypes
+} 
+
+function Get-DirectoryContent($items){
+$array = @()
+foreach ($name in $items){
+    $innername = $name.name 
+    $innerpath = ($path + "\${innername}")
+    $innerpath = [string]$innerpath
+    $itemsize = 0
+    Get-ChildItem -Path $innerpath -Depth 20 -File  -ErrorAction SilentlyContinue -Force|ForEach {$itemsize += $_.Length}
+    if($name.Extension -like ""){
+    $extensionname = ".dir"
+    }else{
+    $extensionname = $name.Extension
+    }
+    $row = ""|Select Name, FileDirectoryExtension, FileDirectorySize
+    $Row.Name = $name.name
+    $Row.FileDirectoryExtension = $extensionname
+    $Row.FileDirectorySize = Get-FileorDirectorySize($itemsize)
+    $array += $Row
+}
+return $array
+}
+
+
+function Get-UsersSize{
+
+$progressbar.Maximum = $starter.Count;
+$progressbar.Step = 1;
+$progressbar.Value = 0;
+
+
+$textBoxDisplay.Text = ""
+foreach ($directory in $starter){
+$outuser = ("--"+$directory.name+" user files--").ToUpper()
+$name = $directory.name
+$path = ($first+"\${name}")
+$path = [string]$path 
+$textBoxDisplay.Text = $textBoxDisplay.Text + ("--"+$outuser+" user files--"+ [Environment]::NewLine)
+$sum = 0
+Get-ChildItem -Path $path -Depth 20 -File  -ErrorAction SilentlyContinue -Force|ForEach {$sum += $_.Length}
+$size = Get-FileorDirectorySize($sum)
+$textBoxDisplay.Text = $textBoxDisplay.Text + ($size.ToString()+ [Environment]::NewLine)
+$progressbar.PerformStep();
+}
+
+}
+
+
+function Get-CountUsersExtensions{
+$progressbar.Maximum = $starter.Count;
+$progressbar.Step = 1;
+$progressbar.Value = 0;
+
+$textBoxDisplay.Text = ""
+foreach ($directory in $starter){
+$outuser = ("--"+$directory.name+" user files--").ToUpper()
+$name = $directory.name
+$path = ($first+"\${name}")
+$path = [string]$path 
+$textBoxDisplay.Text = $textBoxDisplay.Text + ("--"+$outuser+" user files--"+ [Environment]::NewLine)
+
+$filetypes=@{}
+$items = Get-ChildItem -Path $path -Depth 20 -ErrorAction SilentlyContinue -Force
+$out = Count-FileExtensions($items)
+$output = $out.GetEnumerator()|Sort-Object -property Value -Descending|Select Value,Name|Out-String
+
+$textBoxDisplay.Text = $textBoxDisplay.Text + ($output+ [Environment]::NewLine)
+
+$progressbar.PerformStep();
+}
+}
+
+
+function Get-UsersFiles{
+$textBoxDisplay.Text = ""
+$progressbar.Maximum = $starter.Count;
+$progressbar.Step = 1;
+$progressbar.Value = 0;
+foreach ($directory in $starter){
+$outuser = ("--"+$directory.name+" user files--").ToUpper()
+$name = $directory.name
+$path = ($first+"\${name}")
+$path = [string]$path 
+$textBoxDisplay.Text = $textBoxDisplay.Text + ("--"+$outuser+" user files--"+ [Environment]::NewLine)
+$insideDir = ""
+$output = Get-ChildItem -Path $path -ErrorAction SilentlyContinue -Force|Select Name|Out-String 
+$textBoxDisplay.Text = $textBoxDisplay.Text + ("ok"+ [Environment]::NewLine)
+$textBoxDisplay.Text = $textBoxDisplay.Text + ($output+ [Environment]::NewLine)
+$progressbar.PerformStep();
+}
+}
+
+
+function Get-FolderSize{
+$textBoxDisplay.Text = ""
+$progressbar.Maximum = $starter.Count;
+$progressbar.Step = 1;
+$progressbar.Value = 0;
+$counter = 5
+foreach ($directory in $starter){
+$counter -= 1
+if ($counter -gt 0){
+$outuser = ("--"+$directory.name+" user files--").ToUpper()
+$name = $directory.name
+$path = ($first+"\${name}")
+$path = [string]$path 
+$textBoxDisplay.Text = $textBoxDisplay.Text + ("--"+$outuser+" user files--"+ [Environment]::NewLine)
+
+
+$names = Get-ChildItem -Path $path -ErrorAction SilentlyContinue -Force
+$output = Get-DirectoryContent($names)|Format-List Name,FileDirectoryExtension, FileDirectorySize |Out-String
+$textBoxDisplay.Text = $textBoxDisplay.Text + ($output+ [Environment]::NewLine)
+$progressbar.PerformStep();
+}}}
+
+
+
+$ProgressBar = New-Object System.Windows.Forms.ProgressBar
+$ProgressBar.Location = "250, 20"
+$ProgressBar.Size = "650, 30"
+
+$textBoxDisplay = New-Object System.Windows.Forms.TextBox
+$textBoxDisplay.Location = '250, 50'
+$textBoxDisplay.Multiline = $true
+$textBoxDisplay.Name = "textBoxDisplay"
+$textBoxDisplay.Size = '650, 350'
+$textBoxDisplay.Scrollbars = "Both" 
+$textBoxDisplay.TabIndex = 1
+$textBoxDisplay.Readonly = $true
+
+$Size_button = New-object System.windows.forms.button
+$Size_button.text = "Get userfiles size"
+$Size_button.location = '40,50'
+$Size_button.size = '200,40'
+$Size_button.Add_Click({Get-UsersSize})
+
+$GetUserFiles_button = New-object System.windows.forms.button
+$GetUserFiles_button.text = "Get userfolder content"
+$GetUserFiles_button.location = '40,100'
+$GetUserFiles_button.size = '200,40'
+$GetUserFiles_button.Add_Click({Get-UsersFiles})
+
+$CountExtensions_button = New-object System.windows.forms.button
+$CountExtensions_button.text = "Count extensions"
+$CountExtensions_button.location = '40,150'
+$CountExtensions_button.size = '200,40'
+$CountExtensions_button.Add_Click({Get-CountUsersExtensions})
+
+$GetFolderSize_button = New-object System.windows.forms.button
+$GetFolderSize_button.text = "Get userfolders size"
+$GetFolderSize_button.location = '40,200'
+$GetFolderSize_button.size = '200,40'
+$GetFolderSize_button.Add_Click({Get-FolderSize})
+
+$Label = New-Object System.Windows.Forms.Label
+$Label.Location = "60, 20"
+$Label.Size = "200, 40"
+$Label.Text = "Choose option to analyze"
+
+$main_form = New-Object System.Windows.Forms.Form
+$main_form.Text ='User file scanner'
+$main_form.Width = 900
+$main_form.Height = 400
+$main_form.AutoSize = $true
+$main_form.Controls.Add($textBoxDisplay)
+$main_form.Controls.Add($Size_button)
+$main_form.Controls.Add($CountExtensions_button)
+$main_form.Controls.Add($GetUserFiles_button)
+$main_form.Controls.Add($GetFolderSize_button)
+$main_form.Controls.Add($ProgressBar);
+$main_form.Controls.Add($Label);
+$main_form.ShowDialog()
---
+++ courseworkcmdlet.ps1
@@ -0,0 +1,161 @@
+﻿function Get-FileorDirectorySize($item){
+if($item/1GB -ge 1){
+$item = [string]([math]::round(($item/1GB),2)) + " GB"
+}elseif($item/1MB -ge 1)
+{$item = [string]([math]::round(($item/1MB),2)) + " MB"}
+else{$item = [string]([math]::round(($item/1KB),2)) + " KB"}
+return $item
+}
+
+function Count-FileExtensions($items){
+ForEach ($item in $items){
+    if($item.Extension -like ""){
+        if(".dir"  -notin $filetypes.keys){
+            $filetypes[".dir"] = 1 
+        }else{
+            $filetypes[".dir"] += 1}
+    }
+    elseif($item.Extension  -notin $filetypes.keys){
+        $filetypes[$item.Extension] = 1 
+    }else{
+        $filetypes[$item.Extension] += 1
+    }
+}
+return $filetypes
+} 
+
+function Get-DirectoryContent($items){
+$array = @()
+foreach ($name in $items){
+    $innername = $name.name 
+    $innerpath = ($path + "\${innername}")
+    $innerpath = [string]$innerpath
+    $itemsize = 0
+    Get-ChildItem -Path $innerpath -Depth 1 -File  -ErrorAction SilentlyContinue -Force|ForEach {$itemsize += $_.Length}
+    #$itemsize
+    #$toout = $name.name + "${itemsize}"
+    #$toout
+    if($name.Extension -like ""){
+    $extensionname = ".dir"
+    }else{
+    $extensionname = $name.Extension
+    }
+    $row = ""|Select Name, FileDirectoryExtension, FileDirectorySize
+    $Row.Name = $name.name
+    $Row.FileDirectoryExtension = $extensionname
+    $Row.FileDirectorySize = Get-FileorDirectorySize($itemsize)
+    $array += $Row
+}
+return $array
+}
+
+
+function global:Scan-UserFiles{
+<#
+ .SYNOPSIS
+ Commandlet for scanning local user files and getting user files size
+ .DESCRIPTION
+ Commandlet allows user to get info about userfiles virtual size,
+ get info about content of general user folder, similiar to command Get-ChildItem,
+ get what extension files are in user files and amount of each extension type,
+ and get info about general user folder items and sizes of them, that Get-ChildItem can't do.
+ Every output is stored in C:\report.txt if none other report-file name is given.
+ .EXAMPLE  
+ PS C:\> Scan-UserFiles
+ Outputs Usernames in C:\report.txt
+ .EXAMPLE 
+ PS C:\> Scan-UserFiles -FilePath C:\Users\admin\log.txt
+ Outputs Usernames in C:\Users\admin\log.txt
+ .EXAMPLE 
+ PS C:\> Scan-UserFiles -UserFilesSize
+ Outputs Usernames and userfiles size in C:\report.txt
+ .EXAMPLE 
+ PS C:\> Scan-UserFiles -GetUserFilesNames
+ Outputs Usernames and user folder content in C:\report.txt
+ .EXAMPLE 
+ PS C:\> Scan-UserFiles -CountExtensions
+ Outputs Usernames and user files extensions amount in C:\report.txt
+ .EXAMPLE 
+ PS C:\> Scan-UserFiles -GetUserFilesAndSizes
+ Outputs Usernames and user folder items and sizes of every item in C:\report.txt
+ .EXAMPLE 
+ PS C:\> Scan-UserFiles -GetUserFilesNames -CountExtensions -FilePath C:\Users\admin\log.txt
+ Outputs Usernames, user folder content and sizes of every item and user files extensions amount in C:\Users\admin\log.txt
+ .NOTES
+ -GetUserFilesAndSizes processes really long time due to scanning folders over and over, it can take up to 20 minutes to get the result.
+#>
+
+param(
+[switch]$UserFilesSize,
+[switch]$GetUserFilesNames,
+[switch]$CountExtensions,
+[switch]$GetUserFilesAndSizes,
+[parameter(Mandatory =$false)]$FilePath
+)
+
+begin{
+$name = "C:\users"
+$starter = get-childitem -Path $name
+if($Filepath -notmatch ".txt"){
+$Filepath = "c:\report.txt"
+}
+Remove-Item $FilePath -ErrorAction SilentlyContinue
+$count = 0
+$executequeue = @(0,0,0,0,0)
+$noParameterCheck = !($UserFilesSize -or $GetUserFilesNames -or $CountExtensions -or $GetUserFilesAndSizes)
+$buffer = @($UserFilesSize,$GetUserFilesNames,$CountExtensions,$GetUserFilesAndSizes, $noParameterCheck)
+for($num = 0; $num -le 5;$num++){
+if($buffer[$num] -eq $true){
+$executequeue[$num] = $num+1
+}
+}
+}
+
+process{
+foreach($executecheck in $executequeue){
+
+foreach ($directory in $starter){
+#$debugoutuser = '("--"+$directory.name+" user files--").ToUpper()'
+$outuser = '("--"+$directory.name+" user files--").ToUpper()|Out-File -FilePath $Filepath -Append'
+$name = $directory.name
+$path = ($first+"\${name}")
+$path = [string]$path 
+if($executecheck -eq 1){
+Invoke-Expression $outuser
+#Invoke-Expression $debugoutuser
+$sum = 0
+Get-ChildItem -Path $path -Depth 20 -File  -ErrorAction SilentlyContinue -Force|ForEach {$sum += $_.Length}
+$size = Get-FileorDirectorySize($sum)
+$size|Out-File -FilePath $FilePath -Append
+#$size
+""|Out-File -FilePath $FilePath -Append
+}
+if($executecheck -eq 2){
+""|Out-File -FilePath $FilePath -Append
+Invoke-Expression $outuser
+$insideDir = ""
+Get-ChildItem -Path $path -ErrorAction SilentlyContinue -Force|Format-Table LastWriteTime, Name|Out-File -FilePath $FilePath -Append
+}
+if($executecheck -eq 3){
+Invoke-Expression $outuser
+""|Out-File -FilePath $FilePath -Append
+$filetypes = @{}
+$items = Get-ChildItem -Path $path -Depth 20 -ErrorAction SilentlyContinue -Force
+$out = Count-FileExtensions($items)
+$out.GetEnumerator()|Sort-Object -property Value -Descending|Out-File -FilePath $FilePath -Append
+}
+if($executecheck -eq 4){
+Invoke-Expression $outuser
+""|Out-File -FilePath $FilePath -Append
+$names = Get-ChildItem -Path $path -ErrorAction SilentlyContinue -Force
+$array = Get-DirectoryContent($names)
+$array|Out-File -FilePath $FilePath -Append
+}
+if($executecheck -eq 5){
+Invoke-Expression $outuser
+""|Out-File -FilePath $FilePath -Append
+}
+}}
+Start-Process -FilePath $FilePath
+}
+}
